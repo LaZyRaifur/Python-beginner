6f776f472dd3b7b737b3b7b240326671eebc605d
Raifurrahim <raifurrahimcse13@gmail.com>
2020-04-09 23:43:23 +0600

    added list and others

---
+++ List.py
@@ -0,0 +1,24 @@
+# mylist = []
+# mylist.append(1)
+# mylist.append(2)
+# mylist.append(3)
+
+# print(mylist[0])
+# print(mylist[1])
+# print(mylist[2])
+
+# # using loop
+# for x in mylist:
+#     print (x)
+
+number = []
+string = []
+
+number.append(1)
+number.append(2)
+number.append(3)
+
+string.append("hello")
+string.append("Sejuti")
+string.append("Tumi")
+string.append("Haluya khabe")
